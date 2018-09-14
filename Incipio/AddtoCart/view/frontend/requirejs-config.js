9901dc09281ed0db4ca05cc126a92e6795699baa
Isaac Kim <ikim@incipio.com>
2018-09-14 15:59:25 -0700

    M2 AddtoCart from Category page extension init

---
+++ Incipio/AddtoCart/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2017 Incipio
+ * 2017-03 Isaac Kim
+ */
+
+var config = {
+    map: {
+    	'*': {
+    		'mage/SwatchRenderer': 'Magento_Swatches/js/swatch-renderer',
+    		'Magento_Swatches/js/swatch-renderer': 'Incipio_AddtoCart/js/swatch-renderer'
+    	}
+    }
+};
